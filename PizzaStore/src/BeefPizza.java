ea2bacc2a344dc27babd8dca2647c849bf649985
YuXing <a0975708860@gmail.com>
2019-08-28 10:57:51 +0800

    Add new pizza
    Just create a new class, very easy to create

---
+++ PizzaStore/src/BeefPizza.java
@@ -0,0 +1,12 @@
+
+public class BeefPizza implements Pizza{
+	public BeefPizza createPizza() {
+		BeefPizza beefPizza = new BeefPizza();
+		
+//		baking BeefPizza process
+//		...
+//		add double cheese ....
+		
+		return beefPizza;
+	}
+}
--- PizzaStore/src/User.java
+++ PizzaStore/src/User.java
@@ -4,6 +4,7 @@ public class User {
 	public static void main(String[] args) {
 		PizzaStore pizzaStore = new PizzaStore();
 		Pizza nypizza = new NYPizza();
+		@SuppressWarnings("unused")
 		NYPizza bakedPizza = (NYPizza) pizzaStore.order(nypizza);
 //		System.out.println(bakedPizza);
 	}
